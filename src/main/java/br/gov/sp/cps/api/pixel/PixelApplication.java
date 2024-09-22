9a9973b54bce049f6a4993817957ec65631be1f8
Matheus Fiebig <macrubig@gmail.com>
2024-09-22 16:59:46 -0300

    PX-13 - Cors endpoint

--- src/main/java/br/gov/sp/cps/api/pixel/PixelApplication.java
+++ src/main/java/br/gov/sp/cps/api/pixel/PixelApplication.java
@@ -2,6 +2,9 @@ package br.gov.sp.cps.api.pixel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PixelApplication {
@@ -10,4 +13,15 @@ public class PixelApplication {
 		SpringApplication.run(PixelApplication.class, args);
 	}
 
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+			}
+		};
+	}
 }
