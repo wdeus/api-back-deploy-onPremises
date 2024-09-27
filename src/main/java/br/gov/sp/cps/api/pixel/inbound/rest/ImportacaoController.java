ea293535b8d0c06a3251bf4e1ea42cb4cb5c89c6
xxzidanilloxx <wesleydanillo.silva@gmail.com>
2024-09-27 19:57:55 -0300

    PX-12 - Ajuste do endpoint para importação

--- src/main/java/br/gov/sp/cps/api/pixel/inbound/rest/ImportacaoController.java
+++ src/main/java/br/gov/sp/cps/api/pixel/inbound/rest/ImportacaoController.java
@@ -1,12 +1,13 @@
 package br.gov.sp.cps.api.pixel.inbound.rest;
 
-import br.gov.sp.cps.api.pixel.core.domain.dto.command.ImportarCommand;
 import br.gov.sp.cps.api.pixel.core.usecase.AnalisarDocumentoUC;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("importacao")
@@ -15,8 +16,9 @@ public class ImportacaoController {
 
     private final AnalisarDocumentoUC analisarDocumentoUC;
 
+    @SneakyThrows
     @PostMapping
-    public void importar(@RequestBody ImportarCommand document) {
-        analisarDocumentoUC.executar(document.getDocument());
+    public void importar(@RequestParam("file") MultipartFile file) {
+        analisarDocumentoUC.executar(file.getBytes());
     }
 }
