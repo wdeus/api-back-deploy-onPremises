7cd4139cceb17d10dfd57c6f09a6b43bfdf9d5a2
Luciano Pamplona <102562662+lucianonps@users.noreply.github.com>
2024-10-09 20:57:12 -0300

    PX-40 - Criar repository

--- src/main/java/br/gov/sp/cps/api/pixel/core/domain/repository/IndicadorRepository.java
+++ src/main/java/br/gov/sp/cps/api/pixel/core/domain/repository/IndicadorRepository.java
@@ -2,6 +2,11 @@ package br.gov.sp.cps.api.pixel.core.domain.repository;
 
 import br.gov.sp.cps.api.pixel.core.domain.entity.Indicador;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface IndicadorRepository {
     Indicador salvar(Indicador indicador);
+    List<Indicador> listarTodos();
+    Optional<Indicador> buscarID(int id);
 }
