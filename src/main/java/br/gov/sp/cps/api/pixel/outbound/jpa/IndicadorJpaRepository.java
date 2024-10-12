eeb9bb5d12f3a69ad9b89be94426b29577527af4
xxzidanilloxx <wesleydanillo.silva@gmail.com>
2024-10-12 11:29:53 -0300

    PX-37 - Implementação do serviço de envio de email e ajustes no repositório de indicadores

--- src/main/java/br/gov/sp/cps/api/pixel/outbound/jpa/IndicadorJpaRepository.java
+++ src/main/java/br/gov/sp/cps/api/pixel/outbound/jpa/IndicadorJpaRepository.java
@@ -1,7 +1,29 @@
 package br.gov.sp.cps.api.pixel.outbound.jpa;
 
 import br.gov.sp.cps.api.pixel.core.domain.entity.Indicador;
+import br.gov.sp.cps.api.pixel.core.domain.repository.IndicadorRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IndicadorJpaRepository extends JpaRepository<Indicador,Integer> {
+import java.util.List;
+import java.util.Optional;
+
+public interface IndicadorJpaRepository extends JpaRepository<Indicador,Integer>, IndicadorRepository {
+
+    default Indicador salvar(Indicador indicador) {
+        return save(indicador);
+    }
+
+    default List<Indicador> listarTodos() {
+        return findAll();
+    }
+
+    default Optional<Indicador> buscarPorId(int id) {
+        return findById(id);
+    }
+
+    default List<Indicador> localizarPorUsuario(String usuario){
+        return findByUsuario(usuario);
+    }
+
+    List<Indicador> findByUsuario(String usuario);
 }
