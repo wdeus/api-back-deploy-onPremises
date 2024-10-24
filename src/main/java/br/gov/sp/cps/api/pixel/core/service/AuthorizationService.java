149638ddee356a5a885cd1940f1243dbcd338e72
xxzidanilloxx <wesleydanillo.silva@gmail.com>
2024-10-24 15:42:50 -0300

    PX-93 - Implementação da classe de serviço para autorização

---
+++ src/main/java/br/gov/sp/cps/api/pixel/core/service/AuthorizationService.java
@@ -0,0 +1,18 @@
+package br.gov.sp.cps.api.pixel.core.service;
+
+import br.gov.sp.cps.api.pixel.core.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthorizationService {
+
+    private final UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.buscarPorLogin(username);
+    }
+}
